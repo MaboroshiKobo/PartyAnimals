d4b5bc194eda67879732537916b4a123611860de
Muhammad Fardeen <m.fdeen02@gmail.com>
2026-08-03 18:13:55 -0500

    optimize land pathing and fix water speed from compounding too much

--- src/main/java/org/maboroshi/partyanimals/behavior/PinataFloatGoal.java
+++ src/main/java/org/maboroshi/partyanimals/behavior/PinataFloatGoal.java
@@ -13,6 +13,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
 public class PinataFloatGoal implements Goal<Mob> {
+    private static final double[] ANGLES = {45, -45, 90, -90, 135, -135, 180};
     private final Mob mob;
     private final GoalKey<Mob> key;
     private Vector cachedLandDirection = null;
@@ -29,6 +30,12 @@ public class PinataFloatGoal implements Goal<Mob> {
     }
 
     @Override
+    public void start() {
+        cooldown = 0;
+        cachedLandDirection = null;
+    }
+
+    @Override
     public void tick() {
         if (ThreadLocalRandom.current().nextFloat() < 0.8F) {
             Vector velocity = mob.getVelocity();
@@ -37,7 +44,8 @@ public class PinataFloatGoal implements Goal<Mob> {
             }
             Vector dir = getDirectionToLand();
             dir = avoidWalls(dir);
-            velocity.add(dir);
+            velocity.setX(velocity.getX() * 0.5 + dir.getX() * 0.04);
+            velocity.setZ(velocity.getZ() * 0.5 + dir.getZ() * 0.04);
             mob.setVelocity(velocity);
             if (dir.getX() != 0 || dir.getZ() != 0) {
                 Location loc = mob.getLocation();
@@ -51,11 +59,10 @@ public class PinataFloatGoal implements Goal<Mob> {
         Location eyeLoc = mob.getEyeLocation();
         Vector checkDir = intendedDir.clone().normalize();
         if (isBlocked(eyeLoc, checkDir)) {
-            double[] angles = {45, -45, 90, -90, 135, -135, 180};
-            for (double angle : angles) {
+            for (double angle : ANGLES) {
                 Vector rotated = checkDir.clone().rotateAroundY(Math.toRadians(angle));
                 if (!isBlocked(eyeLoc, rotated)) {
-                    return rotated.multiply(0.15);
+                    return rotated;
                 }
             }
         }
@@ -63,7 +70,10 @@ public class PinataFloatGoal implements Goal<Mob> {
     }
 
     private boolean isBlocked(Location origin, Vector direction) {
-        return origin.clone().add(direction).getBlock().getType().isSolid();
+        int targetX = origin.getBlockX() + (int) Math.round(direction.getX());
+        int targetY = origin.getBlockY() + (int) Math.round(direction.getY());
+        int targetZ = origin.getBlockZ() + (int) Math.round(direction.getZ());
+        return origin.getWorld().getBlockAt(targetX, targetY, targetZ).getType().isSolid();
     }
 
     private Vector getDirectionToLand() {
@@ -71,28 +81,39 @@ public class PinataFloatGoal implements Goal<Mob> {
             return cachedLandDirection;
         }
         cooldown = 20;
+
         Location start = mob.getLocation();
-        Block nearestLand = null;
-        double nearestDistSq = Double.MAX_VALUE;
+        int startX = start.getBlockX();
+        int startY = start.getBlockY();
+        int startZ = start.getBlockZ();
+        int bestX = 0;
+        int bestZ = 0;
+        int nearestDistSq = Integer.MAX_VALUE;
         int radius = 8;
-        for (int x = -radius; x <= radius; x += 2) {
-            for (int z = -radius; z <= radius; z += 2) {
-                Block b = start.getWorld().getBlockAt(start.getBlockX() + x, start.getBlockY(), start.getBlockZ() + z);
-                if (b.getType().isSolid() && !b.isLiquid()) {
-                    double distSq = b.getLocation().distanceSquared(start);
-                    if (distSq < nearestDistSq) {
-                        nearestDistSq = distSq;
-                        nearestLand = b;
+        for (int y = -1; y <= 1; y++) {
+            for (int x = -radius; x <= radius; x += 2) {
+                for (int z = -radius; z <= radius; z += 2) {
+                    Block b = start.getWorld().getBlockAt(startX + x, startY + y, startZ + z);
+                    if (b.getType().isSolid() && !b.isLiquid()) {
+                        int distSq = x * x + y * y + z * z;
+                        if (distSq < nearestDistSq) {
+                            nearestDistSq = distSq;
+                            bestX = x;
+                            bestZ = z;
+                        }
                     }
                 }
             }
         }
-        if (nearestLand != null) {
-            cachedLandDirection = nearestLand.getLocation().subtract(start).toVector();
-            cachedLandDirection.setY(0);
-            cachedLandDirection.normalize().multiply(0.15);
+        if (nearestDistSq != Integer.MAX_VALUE) {
+            cachedLandDirection = new Vector(bestX, 0, bestZ);
         } else {
-            cachedLandDirection = start.getDirection().setY(0).normalize().multiply(0.1);
+            cachedLandDirection = start.getDirection().setY(0);
+        }
+        if (cachedLandDirection.lengthSquared() < 0.001) {
+            cachedLandDirection = new Vector(1, 0, 0);
+        } else {
+            cachedLandDirection.normalize();
         }
         return cachedLandDirection;
     }
